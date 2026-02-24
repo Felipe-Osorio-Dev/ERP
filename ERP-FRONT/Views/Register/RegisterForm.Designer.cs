b8536701fa85ecfa1da0a93ee67a3e67604e0923
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-02-23 23:36:18 -0300

    feat: Criado tela para cadastro de produtos no no Sistema.

---
+++ ERP-FRONT/Views/Register/RegisterForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ERP_FRONT.Views.Register
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Cod Produto";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(226, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(244, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Nome";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(244, 33);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(226, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(12, 75);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Quantidade";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 99);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(226, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(244, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Validade";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(244, 99);
+            dateTimePicker1.MinDate = new DateTime(2026, 2, 23, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(226, 23);
+            dateTimePicker1.TabIndex = 7;
+            dateTimePicker1.Value = new DateTime(2026, 2, 23, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(80, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Limpar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(328, 151);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "Cadastrar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // RegisterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 203);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "RegisterForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cadastro de Produto";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Button button2;
+    }
+}
